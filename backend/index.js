b33c0145a0bc4853fde36c0bfa0a022208b41270
mykhailoko <ivashevbrd@gmail.com>
2025-04-02 17:50:19 +0300

    fix api url

--- backend/index.js
+++ backend/index.js
@@ -52,7 +52,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
 
 app.use(cors({
-    origin: ["https://devora-frontend-bco90ozoq-michaels-projects-c4856009.vercel.app"],
+    origin: ["https://devora-frontend-phi.vercel.app/"],
     // http://localhost:5173
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
